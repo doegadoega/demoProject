ff51663e038b552a3eb6abaa22e9140ab0c3e15e
hiroshi egami <doegadoega@hotmail.com>
2023-03-28 13:59:07 +0900

    でもでも

---
+++ DemoApp/DemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAppApp.swift
+//  DemoApp
+//
+//  Created by Hiroshi Egami on 2023/03/28.
+//
+
+import SwiftUI
+
+@main
+struct DemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
